082c42c01b9ef2b54afaa7f83df3b4ba4133a246
zhengxiansheng413 <63422175+zhengxiansheng413@users.noreply.github.com>
2021-01-30 18:31:35 +0800

    Add files via upload

---
+++ vscode/502/index.js
@@ -0,0 +1,4 @@
+function back(){
+    alert("是否跳转返回目录页？");
+    window.location.href="../image/user.html";
+}
---
+++ vscode/download/btn.js
@@ -0,0 +1,319 @@
+function btn1(){
+    var file="http://fffyyy.test.upcdn.net/%E5%88%9D%E4%B8%AD7-9%E5%B9%B4%E7%BA%A7%E7%89%A9%E7%90%86%E6%A0%B8%E5%BF%83%E6%8F%90%E5%88%86%E7%9F%A5%E8%AF%86%E7%82%B9%E5%BD%92%E7%BA%B3%E6%B1%87%E6%80%BB.pdf";
+    document.getElementById('file').style.display="";
+    document.getElementById('file').href=file;
+    var xurl = "http://view.xdocin.com/xdoc?_xdoc=";
+//传入文档地址
+xurl += encodeURIComponent(file);
+//预览参数
+var ops = {
+ 　　// "_pdf": true, //word文档是否以pdf方式显示，默认false
+
+　　// "_watermark": "XDOC文档预览", //水印文本，显示水印
+
+　　// "_saveable": false, //是否允许保存PDF，默认true
+
+　　// "_printable": false, //是否允许打印PDF，默认true
+
+　　// "_copyable": false, //是否允许选择复制内容，默认true
+　　"_toolbar": false,
+　　"_title": "whaleF文件预览", //自定义标题
+};
+//传入预览参数
+for (var a in ops) {
+　　xurl += "&" + encodeURIComponent(a) + "=" + encodeURIComponent(ops[a]);
+    document.getElementById('if').src=xurl;
+}
+}
+//XDOC文档预览服务地址
+function btn2(){
+    var file="http://fffyyy.test.upcdn.net/%E5%88%9D%E4%B8%AD%E7%89%A9%E7%90%86.docx";
+    document.getElementById('file2').style.display="";
+    document.getElementById('file2').href=file;
+    var xurl = "http://view.xdocin.com/xdoc?_xdoc=";
+//传入文档地址
+xurl += encodeURIComponent(file);
+//预览参数
+var ops = {
+ 　　// "_pdf": true, //word文档是否以pdf方式显示，默认false
+
+　　// "_watermark": "XDOC文档预览", //水印文本，显示水印
+
+　　// "_saveable": false, //是否允许保存PDF，默认true
+
+　　// "_printable": false, //是否允许打印PDF，默认true
+
+　　// "_copyable": false, //是否允许选择复制内容，默认true
+　　"_toolbar": false,
+　　"_title": "whaleF文件预览", //自定义标题
+// 　　   //预览链接有效期，单位分钟，默认永久有效
+};
+//传入预览参数
+for (var a in ops) {
+　　xurl += "&" + encodeURIComponent(a) + "=" + encodeURIComponent(ops[a]);
+    document.getElementById('if2').src=xurl;
+}
+}
+//XDOC文档预览服务地址
+function btn3(){
+    var file="http://fffyyy.test.upcdn.net/%EF%BC%88%E8%B7%9F%E8%B0%81%E5%AD%A6%EF%BC%89%E5%88%9D%E4%B8%AD7-9%E5%B9%B4%E7%BA%A7%E7%89%A9%E7%90%86%E7%9F%A5%E8%AF%86%E7%82%B9%E5%BD%92%E7%BA%B3%E6%B1%87%E6%80%BB.doc";
+    document.getElementById('file3').style.display="";
+    document.getElementById('file3').href=file;
+    var xurl = "http://view.xdocin.com/xdoc?_xdoc=";
+//传入文档地址
+xurl += encodeURIComponent(file);
+//预览参数
+var ops = {
+ 　　// "_pdf": true, //word文档是否以pdf方式显示，默认false
+
+　　// "_watermark": "XDOC文档预览", //水印文本，显示水印
+
+　　// "_saveable": false, //是否允许保存PDF，默认true
+
+　　// "_printable": false, //是否允许打印PDF，默认true
+
+　　// "_copyable": false, //是否允许选择复制内容，默认true
+　　"_toolbar": false,
+　　"_title": "whaleF文件预览", //自定义标题 //预览链接有效期，单位分钟，默认永久有效
+};
+//传入预览参数
+for (var a in ops) {
+　　xurl += "&" + encodeURIComponent(a) + "=" + encodeURIComponent(ops[a]);
+    document.getElementById('if3').src=xurl;
+}
+}
+//XDOC文档预览服务地址
+function btn4(){
+    var file="http://fffyyy.test.upcdn.net/%E5%88%9D%E4%B8%AD%E7%89%A9%E7%90%862020%E7%89%88.pptx";
+    document.getElementById('file4').href=file;
+    document.getElementById('file4').style.display="";
+    var xurl = "http://view.xdocin.com/xdoc?_xdoc=";
+//传入文档地址
+xurl += encodeURIComponent(file);
+//预览参数
+var ops = {
+ 　　// "_pdf": true, //word文档是否以pdf方式显示，默认false
+
+　　// "_watermark": "XDOC文档预览", //水印文本，显示水印
+
+　　// "_saveable": false, //是否允许保存PDF，默认true
+
+　　// "_printable": false, //是否允许打印PDF，默认true
+
+　　// "_copyable": false, //是否允许选择复制内容，默认true
+　　"_toolbar": false,
+　　"_title": "whaleF文件预览", //自定义标题
+　　   //预览链接有效期，单位分钟，默认永久有效
+};
+//传入预览参数
+for (var a in ops) {
+　　xurl += "&" + encodeURIComponent(a) + "=" + encodeURIComponent(ops[a]);
+    document.getElementById('if4').src=xurl;
+}
+}
+//XDOC文档预览服务地址
+function btn5(){
+    var file="http://fffyyy.test.upcdn.net/%E4%B8%AD%E8%80%83%E6%95%B0%E5%AD%A6%E5%8E%8B%E8%BD%B4%E9%A2%98100%E9%A2%98%E7%B2%BE%E9%80%89%E3%80%90%E5%90%AB%E7%AD%94%E6%A1%88%E3%80%91.doc";
+    document.getElementById('file5').href=file;
+    document.getElementById('file5').style.display="";
+    var xurl = "http://view.xdocin.com/xdoc?_xdoc=";
+//传入文档地址
+xurl += encodeURIComponent(file);
+//预览参数
+var ops = {
+ 　　// "_pdf": true, //word文档是否以pdf方式显示，默认false
+
+　　// "_watermark": "XDOC文档预览", //水印文本，显示水印
+
+　　// "_saveable": false, //是否允许保存PDF，默认true
+
+　　// "_printable": false, //是否允许打印PDF，默认true
+
+　　// "_copyable": false, //是否允许选择复制内容，默认true
+　　"_toolbar": false,
+　　"_title": "whaleF文件预览", //自定义标题
+　　   //预览链接有效期，单位分钟，默认永久有效
+};
+//传入预览参数
+for (var a in ops) {
+　　xurl += "&" + encodeURIComponent(a) + "=" + encodeURIComponent(ops[a]);
+    document.getElementById('if5').src=xurl;
+}
+}
+//XDOC文档预览服务地址
+function btn6(){
+    var file="http://fffyyy.test.upcdn.net/%E5%88%9D%E4%B8%AD%E6%96%87%E8%A8%80%E6%96%87%E6%B1%87%E6%80%BB.pdf";
+    document.getElementById('file6').href=file;
+    document.getElementById('file6').style.display="";
+    var xurl = "http://view.xdocin.com/xdoc?_xdoc=";
+//传入文档地址
+xurl += encodeURIComponent(file);
+//预览参数
+var ops = {
+ 　　// "_pdf": true, //word文档是否以pdf方式显示，默认false
+
+　　// "_watermark": "XDOC文档预览", //水印文本，显示水印
+
+　　// "_saveable": false, //是否允许保存PDF，默认true
+
+　　// "_printable": false, //是否允许打印PDF，默认true
+
+　　// "_copyable": false, //是否允许选择复制内容，默认true
+　　"_toolbar": false,
+　　"_title": "whaleF文件预览", //自定义标题
+　　   //预览链接有效期，单位分钟，默认永久有效
+};
+//传入预览参数
+for (var a in ops) {
+　　xurl += "&" + encodeURIComponent(a) + "=" + encodeURIComponent(ops[a]);
+    document.getElementById('if6').src=xurl;
+}
+}
+//XDOC文档预览服务地址
+function btn7(){
+    var file="http://fffyyy.test.upcdn.net/%E5%A6%82%E4%BD%95%E9%AB%98%E6%95%88%E9%98%85%E8%AF%BB.pdf";
+    document.getElementById('file7').href=file;
+    document.getElementById('file7').style.display="";
+    var xurl = "http://view.xdocin.com/xdoc?_xdoc=";
+//传入文档地址
+xurl += encodeURIComponent(file);
+//预览参数
+var ops = {
+ 　　// "_pdf": true, //word文档是否以pdf方式显示，默认false
+
+　　// "_watermark": "XDOC文档预览", //水印文本，显示水印
+
+　　// "_saveable": false, //是否允许保存PDF，默认true
+
+　　// "_printable": false, //是否允许打印PDF，默认true
+
+　　// "_copyable": false, //是否允许选择复制内容，默认true
+　　"_toolbar": false,
+　　"_title": "whaleF文件预览", //自定义标题
+　　   //预览链接有效期，单位分钟，默认永久有效
+};
+//传入预览参数
+for (var a in ops) {
+　　xurl += "&" + encodeURIComponent(a) + "=" + encodeURIComponent(ops[a]);
+    document.getElementById('if7').src=xurl;
+}
+}
+//XDOC文档预览服务地址
+function btn8(){
+    var file="http://fffyyy.test.upcdn.net/%E5%88%9D%E4%B8%AD%E8%8B%B1%E8%AF%AD%E5%BF%85%E9%A1%BB%E6%8E%8C%E6%8F%A1%E7%9A%84200%E4%B8%AA%E9%87%8D%E7%82%B9%E5%9B%BA%E5%AE%9A%E6%90%AD%E9%85%8D.pdf";
+    document.getElementById('file8').href=file;
+    document.getElementById('file8').style.display="";
+    var xurl = "http://view.xdocin.com/xdoc?_xdoc=";
+//传入文档地址
+xurl += encodeURIComponent(file);
+//预览参数
+var ops = {
+ 　　// "_pdf": true, //word文档是否以pdf方式显示，默认false
+
+　　// "_watermark": "XDOC文档预览", //水印文本，显示水印
+
+　　// "_saveable": false, //是否允许保存PDF，默认true
+
+　　// "_printable": false, //是否允许打印PDF，默认true
+
+　　// "_copyable": false, //是否允许选择复制内容，默认true
+　　"_toolbar": false,
+　　"_title": "whaleF文件预览", //自定义标题
+　　   //预览链接有效期，单位分钟，默认永久有效
+};
+//传入预览参数
+for (var a in ops) {
+　　xurl += "&" + encodeURIComponent(a) + "=" + encodeURIComponent(ops[a]);
+    document.getElementById('if8').src=xurl;
+}
+}
+//XDOC文档预览服务地址
+function btn9(){
+    var file="http://fffyyy.test.upcdn.net/%EF%BC%88%E8%B7%9F%E8%B0%81%E5%AD%A6%EF%BC%89%E5%88%9D%E4%B8%AD7-9%E5%B9%B4%E7%BA%A7%E8%8B%B1%E8%AF%AD%E7%9F%A5%E8%AF%86%E7%82%B9%E5%BD%92%E7%BA%B3%E6%B1%87%E6%80%BB.doc";
+    document.getElementById('file9').href=file;
+    document.getElementById('file9').style.display="";
+    var xurl = "http://view.xdocin.com/xdoc?_xdoc=";
+//传入文档地址
+xurl += encodeURIComponent(file);
+//预览参数
+var ops = {
+ 　　// "_pdf": true, //word文档是否以pdf方式显示，默认false
+
+　　// "_watermark": "XDOC文档预览", //水印文本，显示水印
+
+　　// "_saveable": false, //是否允许保存PDF，默认true
+
+　　// "_printable": false, //是否允许打印PDF，默认true
+
+　　// "_copyable": false, //是否允许选择复制内容，默认true
+　　"_toolbar": false,
+　　"_title": "whaleF文件预览", //自定义标题
+　　   //预览链接有效期，单位分钟，默认永久有效
+};
+//传入预览参数
+for (var a in ops) {
+　　xurl += "&" + encodeURIComponent(a) + "=" + encodeURIComponent(ops[a]);
+    document.getElementById('if9').src=xurl;
+}
+}
+//XDOC文档预览服务地址
+function btn10(){
+    var file="http://fffyyy.test.upcdn.net/%E4%B8%AD%E8%80%83%E6%95%B0%E5%AD%A6%E5%BF%85%E5%A4%87%E7%9F%A5%E8%AF%86%E7%82%B9%E5%A4%A7%E5%85%A8.pdf";
+    document.getElementById('file10').href=file;
+    document.getElementById('file10').style.display="";
+    var xurl = "http://view.xdocin.com/xdoc?_xdoc=";
+//传入文档地址
+xurl += encodeURIComponent(file);
+//预览参数
+var ops = {
+ 　　// "_pdf": true, //word文档是否以pdf方式显示，默认false
+
+　　// "_watermark": "XDOC文档预览", //水印文本，显示水印
+
+　　// "_saveable": false, //是否允许保存PDF，默认true
+
+　　// "_printable": false, //是否允许打印PDF，默认true
+
+　　// "_copyable": false, //是否允许选择复制内容，默认true
+　　"_toolbar": false,
+　　"_title": "whaleF文件预览", //自定义标题
+　　   //预览链接有效期，单位分钟，默认永久有效
+};
+//传入预览参数
+for (var a in ops) {
+　　xurl += "&" + encodeURIComponent(a) + "=" + encodeURIComponent(ops[a]);
+    document.getElementById('if10').src=xurl;
+}
+}
+//XDOC文档预览服务地址
+function btn11(){
+    var file="http://fffyyy.test.upcdn.net/%E5%88%9D%E4%B8%AD%E7%89%A9%E7%90%86.docx";
+    document.getElementById('file11').href=file;
+    document.getElementById('file11').style.display="";
+    var xurl = "http://view.xdocin.com/xdoc?_xdoc=";
+//传入文档地址
+xurl += encodeURIComponent(file);
+//预览参数
+var ops = {
+ 　　// "_pdf": true, //word文档是否以pdf方式显示，默认false
+
+　　// "_watermark": "XDOC文档预览", //水印文本，显示水印
+
+　　// "_saveable": false, //是否允许保存PDF，默认true
+
+　　// "_printable": false, //是否允许打印PDF，默认true
+
+　　// "_copyable": false, //是否允许选择复制内容，默认true
+　　"_toolbar": false,
+　　"_title": "whaleF文件预览", //自定义标题
+　　   //预览链接有效期，单位分钟，默认永久有效
+};
+//传入预览参数
+for (var a in ops) {
+　　xurl += "&" + encodeURIComponent(a) + "=" + encodeURIComponent(ops[a]);
+    document.getElementById('if11').src=xurl;
+}
+}
+//XDOC文档预览服务地址
+
+
